24037b39e3b76f3b0b8cbe7e591448cb52825eb2
rwxd <rwxd@pm.me>
2022-12-25 16:13:43 +0100

    fix(output): added space

--- wallabag2readwise/output.py
+++ wallabag2readwise/output.py
@@ -6,13 +6,13 @@ class CustomConsole(Console):
         super().__init__(*args, **kwargs)
 
     def print_lvl1(self, text: str):
-        self.print(f'[bold blue]=>{text}[/bold blue]')
+        self.print(f'[bold blue]=> {text}[/bold blue]')
 
     def print_lvl2(self, text: str):
-        self.print(f'[bold green]==>{text}[/bold green]')
+        self.print(f'[bold green]==> {text}[/bold green]')
 
     def print_lvl3(self, text: str):
-        self.print(f'[bold yellow]===>{text}[/bold yellow]')
+        self.print(f'[bold yellow]===> {text}[/bold yellow]')
 
 
 console = CustomConsole()
